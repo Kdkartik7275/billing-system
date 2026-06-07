2dbaca0b4e8e3833b8e035bf5772a382b476c71e
Kartik Dhiman <kd@Kartiks-Mac-mini.local>
2026-06-07 19:13:11 +0530

    Add GitHub Actions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:billing_system/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:billing_system/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
